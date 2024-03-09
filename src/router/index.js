88df9e6befb6990341f95e72c8d17f3c755d36a0
huangjing <huangjing@163.com>
2024-03-09 23:27:44 +0800

    更新vercel.json配置3

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import Router from 'vue-router';
 
 Vue.use(Router);
 const router = new Router({
-  mode: 'history', // history  hash
+  mode: 'hash', // history
   routes: [
     {
       path: '/',
