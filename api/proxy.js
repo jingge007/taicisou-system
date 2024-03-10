1e46abe3ca29666fc7d807a8c0f07438e3c42068
huangjing <huangjing@163.com>
2024-03-10 16:56:30 +0800

    更新vercel.json配置11

--- api/proxy.js
+++ api/proxy.js
@@ -4,7 +4,7 @@ const {createProxyMiddleware} = require('http-proxy-middleware')
 module.exports = (req, res) => {
   let target = ''
   // 代理目标地址
-  // 这里使用 backend 主要用于区分 vercel serverless 的 agentConfig 路径
+  // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
 // xxxxx 替换为你跨域请求的服务器 如： http://baidu.com
   if (req.url.startsWith('/api')) {
     target = 'https://i.maoyan.com'
